c25f7a6fced4b88fbf3b47ad5f1e416928995aa5
Nishimirwe <nishimirwepaul20105@gmail.com>
2021-07-22 23:40:50 +0200

    Adding useefu comments

--- src/Sorts.cpp
+++ src/Sorts.cpp
@@ -21,9 +21,15 @@ void Sorts::append(int d)
     }
 }
 
-void Sorts::bubbleSort() // adaptive sorting algorithm, but we made it using flag variable. By default it is not
-// adaptive, but we can make it adaptive. So, it is known as an adaptive sorting algorithm
+void Sorts::bubbleSort()
 {
+    // adaptive sorting algorithm, but we made it using flag variable. By default it is not
+    // adaptive, but we can make it adaptive. So, it is known as an adaptive sorting algorithm
+
+    //Again, Bubble sort is also a stable sorting Algorithm, as when element are with same value
+    // bubble sort preserve the order of those similar elements.
+
+    // This bubble sort algorithm is not applicable for linked list.
     int s=length+1;
     cout<<"This is Bubble Sort: ";
     int flag=0;
@@ -68,6 +74,9 @@ void Sorts::insertionSort() // insertion sort algorithm
 
     // Another this is that the insertion sort is Stable because when the elements are equal
     // it keeps the order of those elements.
+
+    //Insertions is very suitable for linked list than array. It is meant for linked list, because using arrays
+    // it requires swapping elements for inserting. But for linked lists, no swapping is required, just insert a node where is appropriate.
     cout<<"This is insertion Sort"<<endl;
    int i=length;
    int start=1,j;
@@ -84,3 +93,7 @@ void Sorts::insertionSort() // insertion sort algorithm
        }
    }
 }
+----------------------------------------------------------------------------------------------------------------------
+// UP TO HERE, I WOULD LIKE TO SAY THAT ONLY "BUBBLE SORT AND INSERTION" ARE EVER EXISTING ADAPTIVE SRTING ALGORITHMS |
+// MEANS THAT WHEN ALL ARRAY ELEMENETS ARE ALREADY SORTED, THE PERFORMANCE IS POSOTIVELY AFFECETD.                    |
+----------------------------------------------------------------------------------------------------------------------
