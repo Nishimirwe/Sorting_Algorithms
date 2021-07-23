e40fe76289e9a7261241e0e2094d514c894f2a71
Nishimirwe <nishimirwepaul20105@gmail.com>
2021-07-23 12:57:53 +0200

    no

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 int main()
 {
     Sorts s(50);
-    s.append(50);
-    s.append(70);
-    s.append(60);
-    s.append(90);
-    s.append(40);
+    s.append(3);
+    s.append(7);
+    s.append(9);
     s.append(10);
+    s.append(5);
+    s.append(-1);
+    s.append(44);
+    s.append(31);
+    s.append(17);
+    s.append(12);
+    s.append(4);
+    s.append(11);
+    s.append(2);
+    s.append(34);
     //s.bubbleSort();
     //s.insertionSort();
     //s.selectionSort();
